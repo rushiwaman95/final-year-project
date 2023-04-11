68a862576d32732f89bd9099f9cf41ba0bf22acc
rushiwaman95 <rushikesh.waman@flentas.com>
2023-04-11 15:50:45 +0530

    version 1.0

---
+++ project/demo.py
@@ -0,0 +1,3 @@
+string = "Ram"
+reversed_string = string[::-1]
+print(reversed_string)
---
+++ project/test.py
@@ -0,0 +1,6 @@
+import turtle
+from turtle import*
+from sketchpy import canvas
+bgcolor('black')
+obj=canvas.sketch_from_svg("2023_02_23.svg")
+obj.draw()
